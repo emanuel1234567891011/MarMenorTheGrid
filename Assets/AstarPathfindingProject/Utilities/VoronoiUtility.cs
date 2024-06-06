a7ff60a481c296da6e2d687680883ee2f4ea547f
emanuel1234567891011 <emanuelarnaldos@gmail.com>
2024-06-06 18:49:01 +0200

    Updated

--- Assets/AstarPathfindingProject/Utilities/VoronoiUtility.cs
+++ Assets/AstarPathfindingProject/Utilities/VoronoiUtility.cs
@@ -1,26 +1,26 @@
 using UnityEngine;
+using UnityEngine.Video;
 
 public class VoronoiUtility : MonoBehaviour
 {
     public Texture2D CreateDiagram(Vector2Int dimensions, Vector2Int[] centroids)
     {
-        Texture2D tex = new Texture2D(dimensions.x, dimensions.y);
-
         Color[] regionColors = new Color[centroids.Length];
-        Vector2Int[] testC = new Vector2Int[centroids.Length];
+        Vector2Int[] randomC = new Vector2Int[centroids.Length];
 
         for (int i = 0; i < centroids.Length; i++)
         {
             regionColors[i] = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f), 1f);
-            testC[i] = new Vector2Int(Random.Range(0, dimensions.x), Random.Range(0, dimensions.y));
+            randomC[i] = new Vector2Int(Random.Range(0, dimensions.x), Random.Range(0, dimensions.x));
         }
 
         Color[] pixelColors = new Color[dimensions.x * dimensions.y];
+
         for (int x = 0; x < dimensions.x; x++)
             for (int y = 0; y < dimensions.y; y++)
             {
-                int index = x * dimensions.x + y;
-                pixelColors[index] = regionColors[GetClosestCentroidIndex(new Vector2Int(x, y), testC)];
+                int index = x * dimensions.x + y; //! index is not accounting for non-square textures.
+                pixelColors[index] = regionColors[GetClosestCentroidIndex(new Vector2Int(x, y), randomC)];
             }
 
         return GetImageFromColorArray(dimensions, pixelColors);
